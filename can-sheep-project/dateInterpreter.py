21f8dc017ddc924110d9497752e3d8dd8ab3e6e2
matin mazid <matin_mazid@hotmail.com>
2025-04-13 00:21:00 -0400

    attempt at normalw

--- can-sheep-project/dateInterpreter.py
+++ can-sheep-project/dateInterpreter.py
@@ -1,3 +1,46 @@
+import arrow
+import re
+from datetime import datetime
+from typing import Optional
+# from dateutil import parser
+# from dateutil.relativedelta import relativedelta
+# from dateutil.parser import parse
+# from dateutil.tz import tzlocal
+# from dateutil.tz import tzutc          
 def interpret_date(departure_date:str, arrivate_date:str  )->dict:
+
+    rtn={}
+    # attempt with "Month Day, Year"
+    optionalOfDate=attemptMonthOfyearFormat(departure_date)
+    if optionalOfDate:
+        # If the date string is in the expected format, return the parsed date
+        rtn.update({"arrival_date":optionalOfDate}) 
+        
+
+    optionalOfDate=attemptMonthOfyearFormat(arrivate_date)
+    if optionalOfDate:
+        # If the date string is in the expected format, return the parsed date
+        rtn.update({"departure_date":optionalOfDate})
+
+    # if we have both fields just return now
+    if rtn.get("arrival_date") and rtn.get("departure_date"):
+        return rtn
     
-    return {"a":"b"}
+
+    return rtn
+
+def attemptMonthOfyearFormat(date_string:str)->Optional[tuple]:
+    """
+    Attempt to parse a date string in the format "Month Day, Year" (e.g., "Jan 10, 2015").
+    Returns a tuple containing the month, day, and year as integers.
+    """
+    try:
+        # Attempt to parse the date string
+        parsed_date = datetime.strptime(date_string, "%b %d, %Y")
+        return f"{parsed_date.year}-{parsed_date.month}-{parsed_date.day}"
+    except ValueError:
+        # If parsing fails, return None
+        return None
+
+
+
