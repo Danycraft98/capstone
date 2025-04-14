06a6dd405b62b66e870f8b04cb0790dd4c449886
Daniel Lee <lee.daniel.jhl@gmail.com>
2025-04-13 20:10:32 -0400

    Adding circled text extractor function.

--- poc_sample_code/ocr/main.py
+++ poc_sample_code/ocr/main.py
@@ -1,12 +1,15 @@
 from .got_ocr import get_text
 from .functions import *
+from .circled import extract_circled_texts
 
 def main():
+    image_path = "./Sample Form.png"
+
     # Sample input
-    text = get_text("./Sample Form.png")
+    text = get_text()
     # Extract and display dates
     dates = extract_dates(text)
-    print("📅 Extracted Dates:")
+    print("Extracted Dates:")
     for d in dates:
         print(" -", d)
 
@@ -14,9 +17,16 @@ def main():
 
     # Suggest name corrections
     suggestions = suggest_names(ocr_text)
-    print("\n🧠 Name Suggestions for '{}':".format(ocr_text))
+    print("\nName Suggestions for '{}':".format(ocr_text))
     for name in suggestions:
         print(" -", name)
+        
+    results = extract_circled_texts(image_path, debug=True)
+
+    print("Open Circled Texts Found:")
+    for idx, text in enumerate(results):
+        print(f"{idx+1}: {text}")
+
 
 
 if __name__ == "__main__":
