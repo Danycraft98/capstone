74b88da872fb1f831cf5ad70aec762b7af2e0a74
matin mazid <matinmazid@matins-MacBook-Air.local>
2025-05-15 23:38:41 -0400

    partial fix for gui, we dont duplicate calls to llm

--- can-sheep-project/app.py
+++ can-sheep-project/app.py
@@ -12,25 +12,36 @@ logger = logging.getLogger(__name__)
 data_dict = {}
 # For elements to be displayed in the sidebar, we need to add the sidebar element in the widget.
 # We create a upload input field for users to enter their API key.
-uploaded_file = st.sidebar.file_uploader("Choose an image file")
-if uploaded_file is not None :
-    image=Image.open(uploaded_file)
-    # not thread safe
+st.session_state["clearActive"]=False
 
-    file_path = os.path.join(os.getcwd(), "./tmp/uploaded_file.png")
+uploaded_file = st.sidebar.file_uploader("Choose an image file",accept_multiple_files=False)
+if uploaded_file is not None and uploaded_file.file_id not in st.session_state:
+    st.session_state[uploaded_file.file_id] = True
+    image=Image.open(uploaded_file)
+
+
+    localFileName= f"./tmp/uploaded_file_{uploaded_file.file_id}.png"
+    st.session_state["clearActive"]=True
+    st.session_state["localFileName"] = localFileName
+
+    file_path = os.path.join(os.getcwd(), localFileName)
     image.save(file_path)
+
     file_content= ocr.get_encoded_file(file_path)
     st.sidebar.image(image, caption='Uploaded Image', use_container_width=True)
 
     logging.info(f"Started extracting process at: {datetime.now()}")
     current_date = datetime.now().strftime("%Y-%m-%d")
     # current_date = '2025-04-30'
-    data_dict = functions.translate_text(file_content,current_date)
-    logging.info(f"extract text from image {data_dict}")
+    if "data_dict" not in st.session_state:
+        data_dict = functions.translate_text(file_content,current_date)
+        for key in data_dict:
+            if isinstance(data_dict[key], list):
+                data_dict[key] = " ".join(data_dict[key])
+        st.session_state["data_dict"] = data_dict
+        st.session_state["possible_dates"] = functions.parse_dates(data_dict)
+        logging.info(f"extract text from image {data_dict}")
 
-    for key in data_dict:
-        if isinstance(data_dict[key], list):
-            data_dict[key] = " ".join(data_dict[key])
 st.sidebar.markdown("---")
 
 
@@ -70,23 +81,31 @@ with InfoTab:
 with MainTab:
 
     # Then, we create a intro text for the app, which we wrap in a st.markdown() widget.
-    if uploaded_file is None:
+    # if uploaded_file is None:
+
+    doClear=st.button("clear",disabled=not st.session_state["clearActive"])
+
+    if doClear:
+        os.remove(st.session_state["localFileName"])
+        st.session_state.clear()
+
+    if "data_dict" not in st.session_state:
         st.write("")
         st.markdown("""Upload a scanned form to get the results""")
         st.write("")
 
     else :
         st.subheader("Extracted Data")
-        st.table(data_dict)
-        possibe_dates=functions.parse_dates(data_dict)
-        
+        st.table(st.session_state["data_dict"])
+        possibe_dates = st.session_state["possible_dates"]
         st.subheader("Interpreted Date and Time")
         st.table(possibe_dates)
         logging.info(f"Finished extracting process at: {datetime.now()}")
         saveMe=st.button("Save to MongoDB")
         if saveMe:
-            functions.save_to_mongo(data_dict)
+            functions.save_to_mongo(st.session_state["data_dict"])
             st.success("Data saved to MongoDB successfully!")
+            os.remove(st.session_state["localFileName"])
             st.balloons()
         else:
             st.write("Data not saved to MongoDB") 
